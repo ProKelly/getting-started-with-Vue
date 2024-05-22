1484b6309069222956690a2db20bced80314d74e
prokelly <anyeprincekelly816@gmail.com>
2024-05-21 19:27:49 -0700

    This is the first commit on this project

---
+++ intro/firstsfc/src/App.vue
@@ -0,0 +1,53 @@
+<script>
+  export default{
+    data() {
+      return {message: 'some text is here'};
+    }
+  };
+</script>
+
+<template>
+<h1>
+  {{ message }}
+</h1> 
+<h1>Food</h1>
+<div id="wrapper">
+    <food-item
+      food-name="Apples"
+      food-desc="Apples are a type of fruit that grow on trees."
+      is-fav = 'true'
+      />
+    <food-item
+      food-name="Pizza"
+      food-desc="Pizza has a bread base with tomato sauce, cheese, and toppings on top."
+      is-fav = 'false'
+      />
+    <food-item
+      food-name="Rice"
+      food-desc="Rice is a type of grain that people like to eat."
+      is-fav = 'true'
+      />
+      <food-item
+      food-name="Rice"
+      food-desc="Rice is a type of grain that people like to eat."
+      is-fav = 'true'
+      />
+  </div>
+</template>
+
+<style>
+    
+  #app > div:hover {
+    cursor: pointer;
+  }
+  #wrapper {
+    display: flex;
+    flex-wrap: nowrap;
+  }
+  #wrapper > div {
+    border: dashed black 1px;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+</style>
---
+++ intro/firstsfc/src/components/FoodItem.vue
@@ -0,0 +1,43 @@
+<template>
+    <div v-on:click="countClicks">
+        <h2>{{ foodName }}</h2>
+        <h2>{{ foodDesc }}</h2>
+        <h1>{{ isFav }}</h1>
+        <p id="red">clicked {{ click }} times </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                click: 0
+            }
+        },
+        // props: [
+        //     'foodName',
+        //     'foodDesc',
+        //     'isFav'
+        // ], 
+        props: {
+            foodName: {
+                type: String,
+                required: true
+            },
+            foodDesc: String,
+            isFav: Boolean
+        },
+        methods: {
+            countClicks() {
+                this.click++
+            }
+        }
+    };
+</script>
+
+<style>
+ #red {
+    font-weight: bold ;
+    color: rgb(144, 12, 12);
+  }
+</style>
